3c9b34819f6b23b401d306c980faedeb97218e3a
sadie <sgill@flowwest.com>
2021-04-09 09:59:10 -0700

    reviews analysis and cleans up some things

--- spring_temperature_exceedance/spring_temp_exceedence_analysis.R
+++ spring_temperature_exceedance/spring_temp_exceedence_analysis.R
@@ -1,41 +1,44 @@
 library(readr)
 library(tidyverse)
 library(dplyr)
-library(naniar)
+library(lubridate)
+
+# remotes::install_github("flowwest/waterYearType")
+library(waterYearType)
+
 
 # Load in secchi temp data and add water year, filter to look at correct regions 
 secchi_temp <- read_csv("https://raw.githubusercontent.com/CSAMP/delta-secchi-temperature-data/temp-documentation/delta_water_quality_data_with_strata.csv") 
-filtered_temp <- secchi_temp %>% 
-  mutate(water_year = ifelse(Month >= 8, Year + 1, Year), 
-         Date = lubridate::date(Date)) %>% 
-  filter(Region == c("Yolo Bypass", "Sacramento River")) %>% glimpse() 
+daily_mean_temp <- secchi_temp %>%
+  mutate(date = as_date(Date),
+         water_year = ifelse(month(date) >= 10, year(date) + 1, year(date))) %>% 
+  filter(Region == c("Yolo Bypass", "Sacramento River"), water_year >= 1995) %>% 
+  arrange(water_year) %>% 
+  group_by(water_year, region = Region, date) %>%
+  summarise(mean_daily_temp = mean(Temperature, na.rm = TRUE)) %>%
+  ungroup() %>% 
+  mutate(month = month(date), day = day(date)) %>% 
+  glimpse()
 
-# load in waterYearType data 
-load("~/Git/delta-secchi-temperature-data/frequency_exceeding_temps/water_year_indices.rda") 
-
-water_years <- water_year_indices %>% filter(location == "Sacramento Valley") %>%
-  select(water_year = WY, water_year_type = Yr_type) %>% na.omit() %>% glimpse()
-
-# Filter temp data to daily average temperature measures 
-filtered_temp_with_daily_averages <- filtered_temp %>% 
-  group_by(Region, Date) %>%
-  mutate(mean_daily_temp = mean(Temperature, na.rm = TRUE), count = n()) %>%
-  ungroup() 
+water_years <- waterYearType::water_year_indices %>% 
+  filter(location == "Sacramento Valley", WY >= 1995) %>%
+  select(water_year = WY, water_year_type = Yr_type) %>% 
+  glimpse()
 
 # Join daily aveage temp and water_year data together 
 # Group by water year, water_year_type, region, and month
-temp_with_wy <- filtered_temp_with_daily_averages %>% 
+temp_with_wy <- daily_mean_temp %>% 
   left_join(water_years) %>%
-  filter(Month %in% c(3, 4)) %>%
-  group_by(water_year, water_year_type, Region, Month) %>%
-  mutate(threshold = ifelse(Month == 3, 15, 17)) %>%
-  summarise(prop_above_threshold = mean(Temperature >= threshold),
+  filter(month %in% c(3, 4)) %>% 
+  mutate(threshold = ifelse(month == 3, 15, 17), 
+         exceeded_day = if_else(mean_daily_temp >= threshold, day, as.integer(100))) %>%  
+  group_by(water_year, water_year_type, region, month) %>%
+  summarise(prop_above_threshold = mean(mean_daily_temp >= threshold, na.rm = TRUE),
             n = n(), 
-            min_temp = min(Temperature), 
-            max_temp = max(Temperature), 
-            avg_temp = mean(Temperature),
-            first_day_exceeded =  min(if_else(Temperature  > threshold, Date, ymd('3000-01-01')), na.rm = T)) %>% 
-  mutate(first_day_exceeded = ifelse(first_day_exceeded == ymd('3000-01-01'), NA, day(first_day_exceeded))) %>%
+            min_temp = min(mean_daily_temp, na.rm = TRUE), 
+            max_temp = max(mean_daily_temp, na.rm = TRUE), 
+            mean_temp = mean(mean_daily_temp, na.rm = TRUE),
+            first_day_exceeded = if_else(min(exceeded_day) == 100, as.integer(NA), min(exceeded_day))) %>%  
   gather(stat, value, prop_above_threshold:first_day_exceeded) %>%
   glimpse()
   
