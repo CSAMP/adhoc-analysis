7e304beaedeeaaa69f470378f6ae343d8f885d84
ErinCain <ecain@flowwest.com>
2021-04-09 08:17:56 -0700

    adds comments and removes excess code

--- spring_temperature_exceedance/spring_temp_exceedence_analysis.R
+++ spring_temperature_exceedance/spring_temp_exceedence_analysis.R
@@ -3,26 +3,27 @@ library(tidyverse)
 library(dplyr)
 library(naniar)
 
-# Load in secchi temp data and water year rda from waterYearType repo 
+# Load in secchi temp data and add water year, filter to look at correct regions 
 secchi_temp <- read_csv("https://raw.githubusercontent.com/CSAMP/delta-secchi-temperature-data/temp-documentation/delta_water_quality_data_with_strata.csv") 
 filtered_temp <- secchi_temp %>% 
   mutate(water_year = ifelse(Month >= 8, Year + 1, Year), 
          Date = lubridate::date(Date)) %>% 
   filter(Region == c("Yolo Bypass", "Sacramento River")) %>% glimpse() 
 
+# load in waterYearType data 
 load("~/Git/delta-secchi-temperature-data/frequency_exceeding_temps/water_year_indices.rda") 
 
 water_years <- water_year_indices %>% filter(location == "Sacramento Valley") %>%
   select(water_year = WY, water_year_type = Yr_type) %>% na.omit() %>% glimpse()
 
-# Join them together
-# Select strata and find proportion exceeding in march and april 
+# Filter temp data to daily average temperature measures 
 filtered_temp_with_daily_averages <- filtered_temp %>% 
   group_by(Region, Date) %>%
   mutate(mean_daily_temp = mean(Temperature, na.rm = TRUE), count = n()) %>%
   ungroup() 
 
-
+# Join daily aveage temp and water_year data together 
+# Group by water year, water_year_type, region, and month
 temp_with_wy <- filtered_temp_with_daily_averages %>% 
   left_join(water_years) %>%
   filter(Month %in% c(3, 4)) %>%
@@ -38,11 +39,10 @@ temp_with_wy <- filtered_temp_with_daily_averages %>%
   gather(stat, value, prop_above_threshold:first_day_exceeded) %>%
   glimpse()
   
-
+# View and write csv 
 View(temp_with_wy)
 write_csv(temp_with_wy, "cache_slough_temp_exceeding_thresholds_long.csv")
 # write_csv(temp_with_wy, "cache_slough_temp_exceeding_thresholds_wide.csv")
-# write_csv(temp_with_wy, "frequency_exceeding_temps/cache_slough_strata_temp_exceeding_thresholds.csv")
 
-# Checks a few months to see if proportion exceeding match up 
-# explore_temp <- filtered_temp %>% filter(Year == 2008, Month == 4) %>% mutate(above_17 = sum(Temperature > 17)/n())
+
+
